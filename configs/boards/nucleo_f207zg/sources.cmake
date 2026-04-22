f06eeec716c42146507c48a6d52906ea0d66d880
felix <nqquang@marusysvina.com>
2026-04-21 23:17:02 -0400

    update driver source tree

--- configs/boards/nucleo_f207zg/sources.cmake
+++ configs/boards/nucleo_f207zg/sources.cmake
@@ -1,13 +1,13 @@
 # Board-specific BSP sources and include dirs (included from add_firmware_target).
 
 target_sources(firmware.elf PRIVATE
-  ${CMAKE_SOURCE_DIR}/driver/bsp/common/bsp.c
-  ${CMAKE_SOURCE_DIR}/driver/bsp/device/led/led.c
-  ${CMAKE_SOURCE_DIR}/driver/bsp/device/uart/uart.c
+  ${CMAKE_SOURCE_DIR}/driver/bsp/bsp.c
+  ${CMAKE_SOURCE_DIR}/driver/bsp/led/led.c
+  ${CMAKE_SOURCE_DIR}/driver/bsp/uart/uart.c
 )
 
 target_include_directories(firmware.elf PRIVATE
-  ${CMAKE_SOURCE_DIR}/driver/bsp/common
-  ${CMAKE_SOURCE_DIR}/driver/bsp/device/led
-  ${CMAKE_SOURCE_DIR}/driver/bsp/device/uart
+  ${CMAKE_SOURCE_DIR}/driver/bsp
+  ${CMAKE_SOURCE_DIR}/driver/bsp/led
+  ${CMAKE_SOURCE_DIR}/driver/bsp/uart
 )
