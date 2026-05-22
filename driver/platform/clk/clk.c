d43f80df8a962362c8f130841bdb1b381b4942c9
felix <nqquang@marusysvina.com>
2026-05-22 03:10:17 -0400

    refactor clk.c

--- driver/platform/clk/clk.c
+++ driver/platform/clk/clk.c
@@ -1,16 +1,14 @@
 #include "clk.h"
+
 #include "boards_clk_cfg.h"
 
-static void SystemClock_ApplyBusFromPll(void);
-#if BSP_CLOCK_PLL_SRC == BSP_CLOCK_PLL_SRC_HSE
-static void SystemClock_Config_HSE(void);
-#endif
-#if BSP_CLOCK_PLL_SRC == BSP_CLOCK_PLL_SRC_HSI
-static void SystemClock_Config_HSI(void);
-#endif
-static void SystemClock_Config(void);
+static void clk_halt(void)
+{
+  for (;;) {
+  }
+}
 
-static void SystemClock_ApplyBusFromPll(void)
+static void clk_apply_bus_from_pll(void)
 {
   RCC_ClkInitTypeDef clk = {0};
 
@@ -20,65 +18,42 @@ static void SystemClock_ApplyBusFromPll(void)
   clk.AHBCLKDivider = BSP_CLOCK_AHB_DIV;
   clk.APB1CLKDivider = BSP_CLOCK_APB1_DIV;
   clk.APB2CLKDivider = BSP_CLOCK_APB2_DIV;
+
   if (HAL_RCC_ClockConfig(&clk, BSP_CLOCK_FLASH_LATENCY) != HAL_OK) {
-    for (;;) {
-    }
+    clk_halt();
   }
 }
 
-#if BSP_CLOCK_PLL_SRC == BSP_CLOCK_PLL_SRC_HSE
-static void SystemClock_Config_HSE(void)
+static void clk_configure_osc_pll(void)
 {
   RCC_OscInitTypeDef osc = {0};
-
-  osc.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  osc.HSEState = BSP_CLOCK_HSE_STATE;
-  osc.PLL.PLLState = RCC_PLL_ON;
-  osc.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  osc.PLL.PLLM = BSP_CLOCK_PLLM_HSE;
-  osc.PLL.PLLN = BSP_CLOCK_PLLN;
-  osc.PLL.PLLP = BSP_CLOCK_PLLP;
-  osc.PLL.PLLQ = BSP_CLOCK_PLLQ;
-  if (HAL_RCC_OscConfig(&osc) != HAL_OK) {
-    for (;;) {
-    }
-  }
-  SystemClock_ApplyBusFromPll();
-}
-#endif
 
 #if BSP_CLOCK_PLL_SRC == BSP_CLOCK_PLL_SRC_HSI
-static void SystemClock_Config_HSI(void)
-{
-  RCC_OscInitTypeDef osc = {0};
-
   osc.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   osc.HSIState = RCC_HSI_ON;
   osc.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  osc.PLL.PLLState = RCC_PLL_ON;
   osc.PLL.PLLSource = RCC_PLLSOURCE_HSI;
   osc.PLL.PLLM = BSP_CLOCK_PLLM_HSI;
+#else
+  osc.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  osc.HSEState = BSP_CLOCK_HSE_STATE;
+  osc.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  osc.PLL.PLLM = BSP_CLOCK_PLLM_HSE;
+#endif
+
+  osc.PLL.PLLState = RCC_PLL_ON;
   osc.PLL.PLLN = BSP_CLOCK_PLLN;
   osc.PLL.PLLP = BSP_CLOCK_PLLP;
   osc.PLL.PLLQ = BSP_CLOCK_PLLQ;
-  if (HAL_RCC_OscConfig(&osc) != HAL_OK) {
-    for (;;) {
-    }
-  }
-  SystemClock_ApplyBusFromPll();
-}
-#endif
 
-static void SystemClock_Config(void)
-{
-#if BSP_CLOCK_PLL_SRC == BSP_CLOCK_PLL_SRC_HSI
-  SystemClock_Config_HSI();
-#else
-  SystemClock_Config_HSE();
-#endif
+  if (HAL_RCC_OscConfig(&osc) != HAL_OK) {
+    clk_halt();
+  }
+
+  clk_apply_bus_from_pll();
 }
 
 void clk_init(void)
 {
-  SystemClock_Config();
+  clk_configure_osc_pll();
 }
--- driver/platform/clk/clk.h
+++ driver/platform/clk/clk.h
@@ -1,3 +1,4 @@
 #pragma once
 
+/* SYSCLK/bus from boards_clk_cfg.h (PLL + HSE or HSI). */
 void clk_init(void);
